b822855048a3b258eb1f13a1189edf0998cc0a0f
shubhamj-cuelogic <shubham.jain@cuelogic.co.in>
2017-11-10 18:23:44 +0530

    testing revert

---
+++ AWSUploadShell.sh
@@ -0,0 +1,2 @@
+echo "password" | sudo -S scp -i /home/shubham/Experiment_zone/FTP-SCP-Project/src/lib/cuelogic.pem -r null ubuntu@54.152.13.148:/var/www/html/projects/kumo_reports/2017-11-06/
+echo "Success"
